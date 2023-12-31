146204e00b7e7db3309b58b72d3e6a3e37d19f2c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-01 08:28:26 +1100

    Add CJS test

--- integrations/react/tests/build.test.ts
+++ integrations/react/tests/build.test.ts
@@ -7,11 +7,21 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const rootDir = resolve(__dirname, '..')
 
 const esmExtensions = ['.js', '.js.map', '.d.ts', '.d.ts.map']
+const cjsExtensions = ['.cjs', '.cjs.map', '.d.cts']
 
 describe('Build React', () => {
-  it('should match the output snapshot', () => {
+  it('should build the same ESM output', () => {
     const file = 'esm/index'
     esmExtensions.forEach((ext) => {
+      expect(
+        readFileSync(`${rootDir}/dist/${file}${ext}`).toString(),
+      ).toMatchFileSnapshot(`${rootDir}/snap/${file}${ext}`)
+    })
+  })
+
+  it('should build the same CJS output', () => {
+    const file = 'cjs/index'
+    cjsExtensions.forEach((ext) => {
       expect(
         readFileSync(`${rootDir}/dist/${file}${ext}`).toString(),
       ).toMatchFileSnapshot(`${rootDir}/snap/${file}${ext}`)
