00dc983098fd5175da62a09ff7d79b7930c383ad
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-03 22:29:55 +1100

    ci: Run `test:types` on CI (#9)
    * chore: Add type checking
    
    * Use moduleResolution bundler
    
    * Re-enable strict mode

--- bin/config.js
+++ bin/config.js
@@ -26,9 +26,11 @@ async function requireOrImport(path) {
   try {
     return require(path)
   } catch (e) {
+    // @ts-expect-error
     if (pathToFileURL && e.code === 'ERR_REQUIRE_ESM') {
       // This is needed on Windows, because import() fails if providing a Windows file path.
       const url = pathToFileURL(path)
+      // @ts-expect-error
       return import(url)
     }
     throw e
