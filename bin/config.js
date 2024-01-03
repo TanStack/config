f4b11293221981361400c6eda03eaaa35338454b
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-03 22:28:25 +1100

    Re-enable strict mode

--- bin/config.js
+++ bin/config.js
@@ -26,6 +26,7 @@ async function requireOrImport(path) {
   try {
     return require(path)
   } catch (e) {
+    // @ts-expect-error
     if (pathToFileURL && e.code === 'ERR_REQUIRE_ESM') {
       // This is needed on Windows, because import() fails if providing a Windows file path.
       const url = pathToFileURL(path)
