b84b48008aa4fabb4793c95b843c0a2c31319ace
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-20 09:14:18 +1100

    docs: Document build config options

--- src/build/index.d.ts
+++ src/build/index.d.ts
@@ -1,8 +1,11 @@
 import type { UserConfig } from 'vite'
 
 export type Options = {
+  /** Entry file, e.g. `./src/index.ts` */
   entry: string | string[]
+  /** Source directory used for type generation, e.g. `./src` */
   srcDir: string
+  /** Excluded from type generation, e.g. `[./src/tests]` */
   exclude?: string[]
 }
 
