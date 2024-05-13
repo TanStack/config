e7f758a80d2f098ed69c5d7a3045aaa685c85f59
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-05-13 18:17:37 +1000

    chore: Update packages, enable link-workspace-packages

--- integrations/vue/snap/esm/App.vue.d.ts
+++ integrations/vue/snap/esm/App.vue.d.ts
@@ -1,2 +1,2 @@
-declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}, {}>;
+declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{}>>, {}, {}>;
 export default _default;
