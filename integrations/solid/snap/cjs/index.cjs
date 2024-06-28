27a22233d0dcc41f4618c9fbee41178dd69d455e
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-28 17:18:02 +1000

    ci: Remove solid integration No longer recommended solution

--- integrations/solid/snap/cjs/index.cjs
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const App = require("./App.cjs");
-exports.App = App;
-//# sourceMappingURL=index.cjs.map
--- integrations/solid/snap/esm/index.js
+++
@@ -1,5 +0,0 @@
-import { default as default2 } from "./App.js";
-export {
-  default2 as App
-};
-//# sourceMappingURL=index.js.map
