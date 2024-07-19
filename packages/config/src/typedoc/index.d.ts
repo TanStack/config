56c2e4df152eb5a2a774edccc3a54093d0ed845f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-19 21:03:59 +1000

    Export packages type

--- packages/config/src/typedoc/index.d.ts
+++ packages/config/src/typedoc/index.d.ts
@@ -1,14 +1,16 @@
 import type { UserConfig } from 'vite'
 
+export type Packages = Array<{
+  name: string
+  entryPoints: Array<string>
+  tsconfig: string
+  outputDir: string
+  exclude?: Array<string>
+}>
+
 export type Options = {
   /** Config for packages that need reference docs */
-  packages: Array<{
-    name: string
-    entryPoints: Array<string>
-    tsconfig: string
-    outputDir: string
-    exclude?: Array<string>
-  }>
+  packages: Packages
 }
 
 export function generateReferenceDocs(config: Options): Promise<void>
