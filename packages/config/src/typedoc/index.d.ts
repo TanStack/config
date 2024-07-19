8e41006444cabccf2a3e3b9c92f883ed584a22da
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-19 21:08:24 +1000

    feat(typedoc): Add `generateReferenceDocs` function (#132)
    * feat(typedoc): Add reference doc generation
    
    * Export packages type
    
    * Rename type to Package

---
+++ packages/config/src/typedoc/index.d.ts
@@ -0,0 +1,16 @@
+import type { UserConfig } from 'vite'
+
+export type Package = {
+  name: string
+  entryPoints: Array<string>
+  tsconfig: string
+  outputDir: string
+  exclude?: Array<string>
+}
+
+export type Options = {
+  /** Config for packages that need reference docs */
+  packages: Array<Package>
+}
+
+export function generateReferenceDocs(config: Options): Promise<void>
