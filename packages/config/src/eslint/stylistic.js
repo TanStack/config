8391b19093f51ee49978d9752a7a30edee779dbb
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-26 10:41:21 +1000

    feat: Add @stylistic/eslint-plugin-js

---
+++ packages/config/src/eslint/stylistic.js
@@ -0,0 +1,7 @@
+// https://eslint.style/packages/js
+
+/** @type {import('eslint').Linter.RulesRecord} */
+export const stylisticRules = {
+  /** Enforce consistency of spacing after the start of a comment */
+  '@stylistic/js/spaced-comment': 'error',
+}
