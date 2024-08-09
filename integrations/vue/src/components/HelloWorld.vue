ed499c895755f57f2d331e6ae2bf415205fd6159
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-08-09 19:35:28 +1000

    WIP: try biome and deno fmt

--- integrations/vue/src/components/HelloWorld.vue
+++ integrations/vue/src/components/HelloWorld.vue
@@ -20,8 +20,7 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    >create-vue</a>, the official Vue + Vite starter
   </p>
   <p>
     Install
