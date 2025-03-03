5ca7b1fa45206cb83f95aee4cd784cdc8c1f377b
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-03-03 23:05:33 +1100

    feat(typedoc): release @tanstack/typedoc-config (#220)
    * feat: release @tanstack/typedoc-config
    
    * Fic snap

--- packages/config/src/typedoc/index.d.ts
+++ packages/config/src/typedoc/index.d.ts
@@ -1,18 +1,5 @@
-import type { UserConfig } from 'vite'
-
-export type Package = {
-  name: string
-  entryPoints: Array<string>
-  tsconfig: string
-  outputDir: string
-  exclude?: Array<string>
-}
-
-export type Options = {
-  /** Config for packages that need reference docs */
-  packages: Array<Package>
-  /** Override branch for GitHub links */
-  gitBranch?: string
-}
-
-export function generateReferenceDocs(config: Options): Promise<void>
+export type {
+  Package,
+  Options,
+  generateReferenceDocs,
+} from '@tanstack/typedoc-config'
