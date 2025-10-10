e45558ee9aa3a2f751a175fa0c56d770f6d680dc
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-10-10 15:34:43 -0700

    feat: support for bundling dependencies (#302)
    * feat: support for bundling dependencies
    
    * rename to bundledDeps
    
    * ci: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/vite-config/src/index.d.ts
+++ packages/vite-config/src/index.d.ts
@@ -15,6 +15,8 @@ export type Options = {
   tsconfigPath?: string
   /** Additional dependencies to externalize if not detected by `vite-plugin-externalize-deps` */
   externalDeps?: Array<string | RegExp>
+  /** Dependencies to bundle. Will be passed to the except argument of `vite-plugin-externalize-deps` */
+  bundledDeps?: Array<string | RegExp>
   /** Hook called prior to writing each declaration file; allows to transform the content */
   beforeWriteDeclarationFile?: (filePath: string, content: string) => string
 }
--- packages/vite-config/src/index.js
+++ packages/vite-config/src/index.js
@@ -35,7 +35,10 @@ export const tanstackViteConfig = (options) => {
 
   return defineConfig({
     plugins: [
-      externalizeDeps({ include: options.externalDeps ?? [] }),
+      externalizeDeps({
+        include: options.externalDeps ?? [],
+        except: options.bundledDeps ?? [],
+      }),
       preserveDirectives(),
       tsconfigPaths({
         projects: options.tsconfigPath ? [options.tsconfigPath] : undefined,
