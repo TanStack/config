3de105d7ceba0f6a26ab4946b5bfa530809042e6
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-12-04 08:01:04 +1100

    WIP: use tsdown

---
+++ packages/config/tsdown.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['./src/*.ts'],
+  format: ['esm'],
+  unbundle: true,
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: false,
+  publint: {
+    strict: true,
+  },
+  attw: {
+    profile: 'esm-only',
+    level: 'error',
+  },
+})
