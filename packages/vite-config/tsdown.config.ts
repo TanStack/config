bf3120196904de0b12ac825df1aa9be70002fb73
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2026-01-04 22:29:24 +1100

    build: add platform

--- packages/vite-config/tsdown.config.ts
+++ packages/vite-config/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['esm'],
+  platform: 'node',
   unbundle: true,
   dts: true,
   sourcemap: true,
